97d3cd214cd7aedd7e197dd710f08dace33d5217
YousefDev1 <71141247+YousefDev1@users.noreply.github.com>
2021-01-14 18:29:04 +0200

    Add NavBar => Responsive

--- js/pro.js
+++ js/pro.js
@@ -7,3 +7,16 @@ alert_close.addEventListener('click', ()=> {
         alert_close.parentElement.remove()
     }, 300)
 })
+
+// Toggle List
+var toggle_btn = document.getElementById('toggle-list__btn');
+var navbar_links = document.getElementById('navbar-links');
+
+toggle_btn.addEventListener('click', ()=> {
+    if(navbar_links.style.right == '-50%')
+    {
+        navbar_links.style.right = '0%'
+    }else{
+        navbar_links.style.right = '-50%'
+    }
+})
